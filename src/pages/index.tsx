e004c9f238353f7a15527108e20b61dd7d39df70
Paco Salido <psalido@gmail.com>
2022-12-05 16:43:37 +0100

    Install Plausible stats

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,17 +4,18 @@ import Hero from "@modules/home/components/hero"
 import Layout from "@modules/layout/templates"
 import { ReactElement } from "react"
 import { NextPageWithLayout } from "types/global"
+import PlausibleProvider from "next-plausible"
 
 const Home: NextPageWithLayout = () => {
   return (
-    <>
+    <PlausibleProvider domain="natahome.com">    
       <Head
         title="Home"
         description="Shop all available models only at the Natahome. Worldwide Shipping. Secure Payment."
       />
       <Hero />
       <FeaturedProducts />
-    </>
+    </PlausibleProvider>
   )
 }
 
